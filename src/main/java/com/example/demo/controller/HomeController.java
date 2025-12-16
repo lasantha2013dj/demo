807c694e5520dbe34abfa08fce87ea751c7a225b
lasantha2013dj <lasantha2013d@gmail.com>
2025-12-16 15:45:57 +1100

    update api

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/home")
+@RequestMapping(value = "api/home")
 @CrossOrigin
 public class HomeController {
 
